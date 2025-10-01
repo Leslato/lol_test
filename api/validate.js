1db56e0de7114ff7ee1c82cf9808520e79d4d04b
Leslato <44877915+Leslato@users.noreply.github.com>
2025-10-01 02:58:02 +0200

    Update validate.js

--- api/validate.js
+++ api/validate.js
@@ -1,12 +1,13 @@
 export default function handler(req, res) {
   // On accepte uniquement les GET (mais tu peux élargir si tu veux)
-  if (req.method === "POST") {
+  if (req.method === "GET" || req.method == "POST") {
     const valid = true;
     res.status(200).json({ valid });
   } else {
     res.status(405).json({ error: "Method not allowed" });
   }
 }
+
 
 
 
